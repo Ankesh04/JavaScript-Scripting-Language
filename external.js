fdc007dabc64b164f2ac630205d98b77f7dd607a
Ankesh04 <osankeshagarwal4@gmail.com>
2025-10-22 03:05:29 +0530

    Initial commit: added JavaScript scripting language folder

---
+++ external.js
@@ -0,0 +1,18 @@
+//this is my first code
+/** 
+* *This is a importent comment
+* ! This is warning comment
+* ? This is a question comment
+* TODO: This is a todo comment
+*/
+function buttonclicked(){
+    console.log('Button was Clicked.');
+
+}
+function changetext(){
+    document.getElementById('first').innerHTML='The Para was Changed.';
+    document.getElementsByTagName('h1')[0].style.color = 'red';
+} 
+console.log("This will be executed by node");
+console.log(4+5*20);
+console.error('Just want to see what will happen');
