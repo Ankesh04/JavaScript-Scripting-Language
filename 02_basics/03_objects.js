04259624820494dd701a0062aafbf811efd61c2c
Ankesh Agarwal <osankeshagarwal4@gmail.com>
2025-11-20 21:04:32 +0000

    objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,40 @@
+// singleton
+// Object.create
+
+// object literals
+
+const mySym = Symbol("key1")
+
+
+const JsUser = {
+    name: "Hitesh",
+    "full name": "Hitesh Choudhary",
+    [mySym]: "mykey1",                  //important
+    age: 18,
+    location: "Jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+//console.log(JsUser.full name)     in many case we not use . as it cannot acces this tpe of variables
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
+//console.log(typeof JsUser.mySym)
+
+JsUser.email = "hitesh@chatgpt.com"
+// Object.freeze(JsUser)                    becomes mutable
+JsUser.email = "hitesh@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);     //this is used when you have to use same object
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
