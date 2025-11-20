968581d0a34e5ed5a739efbd279edab9517c8b1e
Ankesh Agarwal <osankeshagarwal4@gmail.com>
2025-11-20 16:44:12 +0000

    object

--- 01_Basic/datatypes-summary.js
+++ 01_Basic/datatypes-summary.js
@@ -32,6 +32,37 @@ const myFunction = function(){
     console.log("Hello world");
 }
 
-console.log(typeof anotherId);
+console.log(typeof anotherId);      //symbol
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+//********************************************** 
+
+// Stack (Primitive), Heap (Non-primitive)
+// whenever stack memory is used the variable declared by you , you get copy of it
+// whenever any variable is in heap memory then you get reference (original value)
+
+let myYoutubename = "Dazzido"
+
+let anotherName = myYoutubename
+anotherName = "Dazzido x"
+
+console.log(myYoutubename);     //Dazzido
+console.log(anotherName);       //Dazzido x
+
+
+let userOne = {
+    email : "user@gmail.com",
+    upi : "user@ybl"
+}
+
+let userTwo = userOne
+ 
+userTwo.email = "dazzido@gmail.com"
+
+console.log(userOne.email);     //dazzido@gmail.com
+console.log(userTwo.email);     //dazzido@gmail.com
+
+//object heap memory me store hota hai to bhale do variable bane
+//but dono ek hi value reference milta hai dono variable me se 
+//koi bhi changes karege to value change ho jayega
