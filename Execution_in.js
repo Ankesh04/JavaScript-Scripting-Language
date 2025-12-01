fcedb7ab66c17f9336c02095def98bf0ede0a1de
Ankesh Agarwal <osankeshagarwal4@gmail.com>
2025-12-01 12:40:02 +0000

    HOW EXECUTION WORKS IN JS

---
+++ Execution_in.js
@@ -0,0 +1,22 @@
+/*
+*********HOW CODE IS EXECUTED IN JAVASCRIPT**********
+
+JAVASCRIPT EXECUTION CONTEXT
+Whatever js file yo have made how it will be executed
+what js does is that it runs in two phase
+
+{}--> this our code 
+so in this Gobal execution context will be made and if the gec is made it will be refered to variable "this" 
+
+interviewer ask if you run js code in browser then browser's gec is different and nodejs environment is different so what we call browser's gec this value ?
+for browser gec this value we call it "window object"
+
+javascript is single threaded iske andar har ek chiz process hoti hai
+
+There are different excecution context:
+-GLOBAL EXECUTION CONTEXT
+-FUNCTION/FUNCTIONAL EXECUTION CONTEXT
+-EVAL EXECUTION CONTEXT
+
+
+*/
