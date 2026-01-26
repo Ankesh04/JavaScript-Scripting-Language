e6ca68f07c1006716049cf032d8484022308b334
Ankesh04 <osankeshagarwal4@gmail.com>
2026-01-26 20:59:23 +0530

    basic

---
+++ script.js
@@ -0,0 +1,25 @@
+/* 
+There is a html file in which there is s body tag and in that we connect html file to js file as
+<script src=””script.js></script>
+
+In script.js file:
+Document.writeln(“I am  ankesh”)
+it is used to write text  in html page 
+Let myname = "ankesh"
+
+document.writeln("i am " + myname + <br/>)
+
+*/
+//calculator
+let num1 =80
+let num2 =12
+
+document.writeln("addition of 2 numbers: " +(num1+num2) +"<br>")
+document.writeln("substraction of 2 numbers: " +(num1-num2) +"<br>")
+document.writeln("multiplication of 2 numbers: " +num1*num2 +"<br>")
+document.writeln("division of 2 numbers: " +num1/num2 +"<br>")
+
+/*
+
+
+*/
---
+++ variables.js
@@ -0,0 +1,20 @@
+//defining a variable
+let noOfStudent = 5;
+let welcomeMessage = "hello world" ;
+console.log(noOfStudent);
+//updating a variable
+let money = 1;
+money += 5;
+money -= 2;
+console.log(money);
+//ways to create a varible
+//1.VAR
+var fruit = "apple";
+//we can swap or cahnge the value from anywhere
+//2.let
+let fruit = "mango";
+//we can change but value but it has ristriction
+//3.const
+const fruit1 ="banana";
+//we cannot cahnge the value from anywhere
+//example of all
