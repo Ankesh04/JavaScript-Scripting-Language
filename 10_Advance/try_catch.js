0a7cecd9e03683ed29a6dadd04ef855546728469
Ankesh04 <osankeshagarwal4@gmail.com>
2026-03-31 07:15:44 +0530

    try catch

---
+++ 10_Advance/try_catch.js
@@ -0,0 +1,11 @@
+/*TRY & CATCH
+the try statement allows you to define a block of code to be tested for errors while it is being executed.
+
+the cath statement allows you to define a block of code to be executed, if an error occurs in the try block.
+
+*/
+try{
+    console.log(a);
+}catch {
+    console.log("variable a doesnot exist");
+}
