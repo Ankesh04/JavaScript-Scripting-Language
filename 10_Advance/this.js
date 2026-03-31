b7a2897e299768e04658cf92c3fb059524ab5ec8
Ankesh04 <osankeshagarwal4@gmail.com>
2026-03-31 06:55:57 +0530

    this keyword

---
+++ 10_Advance/this.js
@@ -0,0 +1,18 @@
+/*THIS keyword
+it refers to an object that is executing the current piece of code.
+window object is the default function in the javascript browser
+*/
+
+const student ={
+    name : "aneksh",
+    age:23,
+    eng:90,
+    math:98,
+    phy: 91,
+    getAvg(){
+        let avg = (this.eng + this.math + this.phy)/3;
+        console.log(avg);
+        //we have used this keyword to take variable from the object
+        //this represent the object which is been called
+    }
+}
