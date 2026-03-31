6a922206e19077eea133a47282eb89abfab29061
Ankesh04 <osankeshagarwal4@gmail.com>
2026-03-31 07:32:08 +0530

    spread

---
+++ 10_Advance/spread.js
@@ -0,0 +1,12 @@
+/*
+SPREAD
+expands an iterable into multiples vlaues
+*/ 
+
+function fucn (...arr){
+    //do something
+}
+
+let arr=[1,2,1,1,3,3,1,3,1]
+let arr2 = [1,3,5,6,6,7,6]
+console.log(...arr2, ...arr);
