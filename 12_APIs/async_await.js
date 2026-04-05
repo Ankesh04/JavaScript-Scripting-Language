3160b88abd9189a7d4831455a64e39827efcdd8f
Ankesh04 <osankeshagarwal4@gmail.com>
2026-04-05 05:47:11 +0530

    async_await in api

---
+++ 12_APIs/async_await.js
@@ -0,0 +1,16 @@
+let url = "https://catfact.ninja/fact";
+
+async function getFacts() {
+    let res = fetch(url);
+    console.log(res);
+}
+
+//here the console.log() will be printed but the api response will be undefined 
+// as js works asynchonous so it doesnot wait
+
+async function getFacts() {
+    let res = await fetch(url);//here it will wait until the response will come from the api
+    let data = await res.json();
+    console.log(res);
+    console.log(data.fact);
+}
