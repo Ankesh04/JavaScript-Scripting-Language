1507714ef4acd58a50c481869adedbc22360461d
Ankesh04 <osankeshagarwal4@gmail.com>
2026-08-24 17:33:24 +0530

    Revised decleartion in js

---
+++ Revision/day1/1_script.js
@@ -0,0 +1,29 @@
+console.log("Good morning");
+//clg shorcut for console.log()
+var a;
+var a = 10;
+let b = 20;
+const c = 30;
+console.log(a);
+console.log(b);
+console.log(c);
+//VAR
+a = 20;
+var a = 30;
+console.log(a);
+
+//LET
+let k;
+// let k //X cannot be redeclared
+k = 10;
+k = 20;
+// let k = 30 X
+console.log(k);
+
+//CONST
+// const a  cannot be only declared
+const d = 10;
+// const d cannot be re declared if done before
+//  d=20 cannot be re initialised
+// const d =30  cannot be re decalred and reinitialised
+console.log(d);
