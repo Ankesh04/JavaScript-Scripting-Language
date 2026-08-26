7ed45cf6901199d182377597e69c9decbc5a98a0
Ankesh04 <osankeshagarwal4@gmail.com>
2026-08-26 09:56:19 +0530

    revised operators

--- Revision/day1/1_script.js
+++ Revision/day1/1_script.js
@@ -1,29 +1,74 @@
-console.log("Good morning");
-//clg shorcut for console.log()
-var a;
-var a = 10;
-let b = 20;
-const c = 30;
-console.log(a);
-console.log(b);
-console.log(c);
-//VAR
-a = 20;
-var a = 30;
-console.log(a);
+// console.log("Good morning");
+// //clg shorcut for console.log()
+// var a;
+// var a = 10;
+// let b = 20;
+// const c = 30;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// //VAR
+// a = 20;
+// var a = 30;
+// console.log(a);
 
-//LET
-let k;
-// let k //X cannot be redeclared
-k = 10;
-k = 20;
-// let k = 30 X
-console.log(k);
+// //LET
+// let k;
+// // let k //X cannot be redeclared
+// k = 10;
+// k = 20;
+// // let k = 30 X
+// console.log(k);
 
-//CONST
-// const a  cannot be only declared
-const d = 10;
-// const d cannot be re declared if done before
-//  d=20 cannot be re initialised
-// const d =30  cannot be re decalred and reinitialised
-console.log(d);
+// //CONST
+// // const a  cannot be only declared
+// const d = 10;
+// // const d cannot be re declared if done before
+// //  d=20 cannot be re initialised
+// // const d =30  cannot be re decalred and reinitialised
+// console.log(d);
+
+//TYPECASTING
+// implicit
+// let s = 10;
+// let h = "20";
+// console.log(s + h);
+// console.log(s - h);
+// console.log(s * h);
+// console.log(s * h);
+// console.log(h / s);
+// //explicit
+// let f = "25";
+// let ab = Number(f);
+// console.log(ab);
+// console.log(typeof ab);
+
+// to take value from user
+// let age = Number(prompt("Enter the age:"));
+// console.log(age);
+
+// //Arithematic Operqator
+// let num1 = Number(prompt("Enter the First number:"));
+// let num2 = Number(prompt("Enter the Second number:"));
+// let addition = num1 + num2;
+// let difference = num1 - num2;
+// let product = num1 * num2;
+// let division = num1 / num2;
+// let reminder = num1 % num2;
+// let power = num1 ** num2;
+// console.log("The addition =", addition);
+// console.log("The difference =", difference);
+// console.log("The product =", product);
+// console.log("The devision = ", division);
+// console.log("The reminder =", reminder);
+// console.log("The power product =", power);
+
+// Comparision operator
+console.log(10 <= 10);
+console.log(10 == 10);
+console.log(10 == "10");
+console.log(10 == "100");
+console.log("qwd", 10 !== "10"); //check the data type which is not true
+console.log("qdwd", 10 !== "100"); //check the data type which is not true
+console.log(10 === 10);
+console.log(10 === "10");
